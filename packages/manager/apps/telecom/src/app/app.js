eb6d67a8263d7ab8e3133975b9cbfdd8c29fffb7
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-11-14 16:36:18 +0100

    refactor(telecom.telephony): add abstract telephony state
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/telecom/src/app/app.js
+++ packages/manager/apps/telecom/src/app/app.js
@@ -36,7 +36,7 @@ import ngOvhContact from '@ovh-ux/ng-ovh-contact';
 
 import uiRouter, { RejectType } from '@uirouter/angularjs';
 import TelecomAppCtrl from './app.controller';
-import carrierSip from './telecom/telephony/carrierSip';
+import telephony from './telecom/telephony';
 
 import errorPage from './error-page/error-page.module';
 import navbar from '../components/navbar';
@@ -116,7 +116,7 @@ angular.module('managerApp', [
   'ui.sortable',
   'ui.validate',
   'validation.match',
-  carrierSip,
+  telephony,
   portabilities,
 ])
 
